3c42c43f0d3421058e1295dbe7479d3e735baaf5
dareversat <david.reversat@upmc.fr>
2015-12-07 13:54:16 +0100

    remove v42

--- v42/installeurs/src/Windows/createExe.bat
+++
@@ -1,3 +0,0 @@
-MakeExeFromBat.bat SyncPy-X.X.bat wget.exe 7za.exe 7za.dll 7zxa.dll libeay32.dll libiconv2.dll libintl3.dll libssl32.dll
-
-pause
--- v42/src/DataFromManyPersons/Univariate/Continuous/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This package allows to compute synchronisation between continuous monovariate signals gathered 
-from many persons. 
-"""
-
-__all__ = ['Linear', 'Nonlinear', 'MachineLearning' ]
