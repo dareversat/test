dcdcbca805af19762f887c7fca6b6529df442f3a
dareversat <david.reversat@upmc.fr>
2015-12-07 14:24:03 +0100

    remove v42

--- v42/src/DataFrom2Persons/Univariate/Categorical/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This package allows to compute synchronisation between categorical monovariate signals gathered 
-from two persons. 
-"""
-
-__all__ = ['Linear', 'Nonlinear', 'MachineLearning' ]
--- v42/src/DataFromManyPersons/Univariate/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This package allows to compute synchronisation between monovariate signals gathered 
-from many persons. 
-"""
-
-__all__ = ['Categorical', 'Continuous']
